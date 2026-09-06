e04e4e45fecd27a1f32828f282b2bc0b85245fd3
alirezagh99 <agh102724@gmail.com>
2026-09-07 02:48:23 +0330

    added sign up

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
+import { jwt } from "zod";
 
 export const config = {
   pages: {
@@ -51,13 +52,33 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // set the user ID from the token
       session.user.id = token.sub;
-
+      session.user.role = token.role;
+      session.user.name = token.name;
       // if there is an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
 
       return session;
+    },
+    async jwt({ token, user, session, trigger }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // if user has no name then use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      return token;
     },
   },
 };

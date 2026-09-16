a26c93a1bce6410e6388c61fcd39d164b1c4a6f9
alirezagh99 <agh102724@gmail.com>
2026-09-16 20:08:30 +0330

    asxs

--- app/email/index.tsx
+++ app/email/index.tsx
@@ -9,10 +9,12 @@ import PurchaseReceiptEmail from "./purchase-receipt";
 const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
-  await resend.emails.send({
+  const res = await resend.emails.send({
     from: `${APP_NAME} <${SENDER_EMAIL}>`,
     to: order.user.email,
     subject: `Order Confirmation ${order.id}`,
     react: <PurchaseReceiptEmail order={order} />,
   });
+
+  console.log("resresresresres:", res);
 };
